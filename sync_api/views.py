d507c38f9f2eba1770b323f8b55218fa9ccdfcf2
Asus <ma4301511@gmail.com>
2025-11-06 08:58:28 +0330

    aaaaa

--- sync_api/views.py
+++ sync_api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.apps import apps
 from django.utils import timezone
+from .models import ChangeTracker
 import decimal
 
 
