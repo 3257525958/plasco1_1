f1ba9c81bf9405e3a0fa2f4a2b97d46ec0dc19cd
Asus <ma4301511@gmail.com>
2025-11-11 11:21:08 +0330

    دسترسی همه

--- ip_manager/urls.py
+++ ip_manager/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import manage_ips, list_ips, add_ip, delete_ip, update_ip, toggle_ip
 
 urlpatterns = [
-    path('ip_manager/', manage_ips, name='manage_ips'),  # این میشه: /ip/ip_manager/
-    path('ip_manager/api/list/', list_ips, name='list_ips'),  # این میشه: /ip/ip_manager/api/list/
-    path('ip_manager/api/add/', add_ip, name='add_ip'),  # این میشه: /ip/ip_manager/api/add/
-    path('ip_manager/api/delete/<int:ip_id>/', delete_ip, name='delete_ip'),  # این میشه: /ip/ip_manager/api/delete/1/
-    path('ip_manager/api/update/<int:ip_id>/', update_ip, name='update_ip'),  # این میشه: /ip/ip_manager/api/update/1/
-    path('ip_manager/api/toggle/<int:ip_id>/', toggle_ip, name='toggle_ip'),  # این میشه: /ip/ip_manager/api/toggle/1/
+    path('', manage_ips, name='manage_ips'),  # حذف ip_manager/ از ابتدا
+    path('api/list/', list_ips, name='list_ips'),
+    path('api/add/', add_ip, name='add_ip'),
+    path('api/delete/<int:ip_id>/', delete_ip, name='delete_ip'),
+    path('api/update/<int:ip_id>/', update_ip, name='update_ip'),
+    path('api/toggle/<int:ip_id>/', toggle_ip, name='toggle_ip'),
 ]
