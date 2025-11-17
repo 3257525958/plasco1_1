23e83deb3b4697beeecd96bbfd0f7b63ca76d81f
Asus <ma4301511@gmail.com>
2025-11-17 07:37:24 +0330

    zip

--- ip_manager/views.py
+++ ip_manager/views.py
@@ -296,7 +296,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        'cantact_app.middleware.AdvancedSessionMiddleware',
 
 ]
 
